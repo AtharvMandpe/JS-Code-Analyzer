e8c932d45f57c2b69da9769b00518108bcf5d7a0
Atharv Mandpe <atharvmandpe2@gmail.com>
2024-10-20 20:35:31 +0530

    lexical analysis

---
+++ test.js
@@ -0,0 +1,13 @@
+var x = 10;
+let y = 20;
+const z = x + y;
+
+if (x > y) {
+    console.log("x is greater than y");
+} else {
+    console.log("x is less than or equal to y");
+}
+
+for(var i=0; i<10 ;i++){
+    console.log(i);
+}
